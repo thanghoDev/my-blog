54ff12970bd69346553dbffd760bc104a2c152b0
Thang Ba Ho <thang.ho@asnet.com.vn>
2022-03-18 17:46:39 +0700

    feat: add new example react hooks

---
+++ example-hooks/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import UseState from "./component/useState";
+function App() {
+	return (
+		<React.Fragment>
+			<UseState />
+		</React.Fragment>
+	);
+}
+
+export default App;
