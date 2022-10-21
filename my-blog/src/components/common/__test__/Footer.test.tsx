5b8989fa614835dd846840e218b0d56d3d8cc4b9
thangho <thang.ho@asnet.com.vn>
2022-10-21 10:05:20 +0700

    feat: add menu and custom document page

--- my-blog/src/components/common/__test__/Footer.test.tsx
+++ my-blog/src/components/common/__test__/Footer.test.tsx
@@ -1,6 +1,5 @@
 import Footer from '../Footer';
 import { render, screen } from '@testing-library/react';
-import '@testing-library/jest-dom';
 
 describe('Footer', () => {
   test('renders a footer', () => {
--- my-blog/src/components/layout/index.tsx
+++ my-blog/src/components/layout/index.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import { ReactNode } from 'react';
 import Footer from '../common/Footer';
 import Header from '../common/Header';
@@ -10,13 +9,6 @@ type LayoutProps = {
 function Layout({ children }: LayoutProps) {
   return (
     <>
-      <Head>
-        <meta charSet='UTF-8' />
-        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
-        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-        <title>My Blog</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
       <div className='container'>
         <Header />
         <main>{children}</main>
