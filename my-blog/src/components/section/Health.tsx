e010a82c055e2f36833674902b2d9aedc7dab86b
thangho <thang.ho@asnet.com.vn>
2022-10-25 17:28:38 +0700

    feat: add detail page UI

--- my-blog/src/components/section/Health.tsx
+++ my-blog/src/components/section/Health.tsx
@@ -4,6 +4,7 @@ import { Col, Row } from 'react-bootstrap';
 
 // types
 import { Blog } from '@/types/blog';
+import Link from 'next/link';
 
 type HealthProps = {
   health: Blog[];
@@ -14,7 +15,11 @@ function Health({ health }: HealthProps) {
 
   return (
     <Row md={8} className='mt-5 align-content-start'>
-      <h2 className='mb-5 text-decoration-underline fs-5'>Health</h2>
+      <Link href={`category/${firstArticle.category}`}>
+        <h2 className='mb-5 postTitle text-decoration-underline text-capitalize fs-5'>
+          <a>{firstArticle.category}</a>
+        </h2>
+      </Link>
       <Col md={6}>
         <div className='mb-2'>
           <Image
@@ -24,9 +29,12 @@ function Health({ health }: HealthProps) {
             alt={firstArticle.alt}
           />
           <div className='mt-5'>
-            <h2 className='fs-5 text-black text-capitalize'>
-              {firstArticle.title}
-            </h2>
+            <Link href={`detail/${firstArticle.id}`}>
+              <a className='fs-5 text-black text-decoration-none text-capitalize'>
+                {firstArticle.title}
+              </a>
+            </Link>
+
             <p className='description'>{firstArticle.description}</p>
           </div>
         </div>
@@ -36,10 +44,18 @@ function Health({ health }: HealthProps) {
           <div key={item.id} className='d-flex mb-2'>
             <Image width='105' height='107' src={item.images} alt={item.alt} />
             <div className='ms-4'>
-              <h2 className='fs-6 text-black text-capitalize'>{item.title}</h2>
-              <p className='text-capitalize category'>
+              <Link href={`detail/${item.id}`}>
+                <a className='fs-6 text-black text-decoration-none text-capitalize'>
+                  {item.title}
+                </a>
+              </Link>
+              <p className='text-capitalize category mt-3'>
                 Dave Rogers <code className='text-secondary'>in</code>{' '}
-                {item.category}
+                <Link href={`category/${item.category}`}>
+                  <a className='text-black text-decoration-none'>
+                    {item.category}
+                  </a>
+                </Link>
               </p>
             </div>
           </div>
--- my-blog/src/components/section/PostList.tsx
+++ my-blog/src/components/section/PostList.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from 'next/future/image';
 import { Col } from 'react-bootstrap';
 import { Blog } from '@/types/blog';
+import Link from 'next/link';
 
 type PostListProps = {
   data: Blog[];
@@ -11,9 +12,12 @@ type PostListProps = {
 function PostList({ data, title }: PostListProps) {
   return (
     <Col md={6} className='me-4'>
-      <h2 className='mb-5 text-decoration-underline text-capitalize fs-5'>
-        {title}
-      </h2>
+      <Link href={`category${title}`}>
+        <h2 className='mb-5 text-decoration-underline text-capitalize fs-5 postTitle'>
+          <a>{title}</a>
+        </h2>
+      </Link>
+
       {data.map((item) => (
         <div key={item.id} className='d-flex mb-3'>
           <Image width='165' height='180' src={item.images} alt={item.alt} />
