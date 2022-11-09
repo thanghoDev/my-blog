d82d77439904e7107f8d501103ea57794f16e87c
Thang Ho <thang.ho@asnet.com.vn>
2022-11-09 17:28:23 +0700

    refactor: refactor fetch posts function and someone files

--- my-blog/src/pages/_app.tsx
+++ my-blog/src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import type { AppProps } from 'next/app';
-import Layout from '@/components/Layout';
+import Layout from '@/components/layout';
 import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
